f736ee93f56f3e4c67b3ead06273ccec0acc3371
Jeffrey <j.lo128456@gmail.com>
2025-05-06 21:34:22 +1000

    updated css

--- frontend/src/components/PlanFinder.js
+++ frontend/src/components/PlanFinder.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import "./PlanFinder.css"; // optional if you want to extract styles
+
 
 function PlanFinder() {
   const [plans, setPlans] = useState([]);
